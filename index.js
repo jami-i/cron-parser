dba146cb17fd9991e8dd70478fcd34e33fa13ae0
Osamu Ishikawa <jami-i@users.noreply.github.com>
2019-12-04 00:08:29 +0900

    Implement minute parser.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const P = require('parsimmon');
+const minuteValidatorFactory = require('./lib/minute');
 
-const res = P.string("a").parse("a");
 
-console.log(res);
+const validator = minuteValidatorFactory('1-3,5-6');
+
+console.log(validator('3'));
+console.log(validator('4'));
+console.log(validator('5'));
 
